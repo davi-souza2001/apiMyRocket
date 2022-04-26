9b4dd8ddea7e211bcc09f67b4626194651b07c41
davi-souza2001 <davisouza2001dv@gmail.com>
2022-04-26 19:53:45 -0300

    up

--- dist/data/client.js
+++
@@ -1,7 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _axios = require('axios'); var _axios2 = _interopRequireDefault(_axios);
-
-const api = _axios2.default.create({
-    baseURL: "https://api.github.com/users"
-});
-
-exports. default = api;
--- dist/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";var _app = require('./app');
-
-_app.app.listen(process.env.PORT || 5000, () => {
-    console.log('Server Running')
-})
